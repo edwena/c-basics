c74c87a60f85276e959640ef4e9ed1d22e7d9575
Nelly Demchenko <moi93@mail.ru>
2017-05-15 23:14:40 +0300

    new folder
    plus small task

---
+++ src/04_data_types/08_int_num/a_rabbits.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int max_rabbits_in_cage(int cages, int rabbits) {
+    int per_cage = 0;
+    per_cage = rabbits / cages;
+    if (rabbits % cages != 0) {
+        per_cage++;
+    }
+    return per_cage;
+}
+
+// Условия задания:
+// Пусть есть n клеток и m зайцев, которых рассадили по этим клеткам.
+// Вам требуется расcчитать максимальное количество зайцев, которое гарантированно окажется в одной клетке.
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+    printf("%d\n", max_rabbits_in_cage(n, m));
+    return 0;
+}
